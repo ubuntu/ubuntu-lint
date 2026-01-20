7b1ffa311fea76750fe3d9451097358a7962b10f
Nick Rosbrook <enr0n@ubuntu.com>
2026-01-20 15:48:20 -0500

    context: add initial Context class
    The purpose of this class is to encapsulate context for package source
    changes, the source package, etc. required for a linter to operate.
    
    A linter is intended to be a function that accepts a Context, and using
    the available information, determines if the check has passed or not. If
    it fails, it calls Context.lint_fail with the reason, which raises
    LintFailure.
    
    Callers of the linters should handle LintFailure to determine why the
    check failed.
    
    For now, the class contains a changes attribute (type debian.deb822.Changes)
    to inspect a source changes file, and a debian_changelog attribute
    (type debian.changelog.Changelog) to inspect the changelog of a source
    package.
    
    More information will be added to this class as needed.

---
+++ ubuntu_lint/context.py
@@ -0,0 +1,66 @@
+from debian import deb822, changelog
+
+
+class LintFailure(Exception):
+    """
+    This exception is raised when a linter calls Context.lint_fail. Callers of
+    linters should handle this exception to determine why a specific linter
+    failed.
+    """
+
+    pass
+
+
+class Context:
+    """
+    A class to encapsulate the context of a source package, or package upload
+    for a linter. Instances of Context are passed to linters.
+    """
+
+    def __init__(
+        self,
+        changes: str | deb822.Changes | None = None,
+        debian_changelog: str | changelog.Changelog | None = None,
+    ):
+        self._changes: deb822.Changes = None
+        if isinstance(changes, str):
+            with open(changes, "r") as f:
+                self._changes = deb822.Changes(f)
+
+        elif isinstance(changes, deb822.Changes):
+            self._changes = changes
+
+        elif changes is not None:
+            raise ValueError("invalid type for changes")
+
+        self._changelog: changelog.Changelog = None
+        if isinstance(debian_changelog, str):
+            with open(debian_changelog, "r") as f:
+                self._changelog = changelog.Changelog(f)
+
+        elif isinstance(debian_changelog, changelog.Changelog):
+            self._changelog = debian_changelog
+
+        elif debian_changelog is not None:
+            raise ValueError("invalid type for changelog")
+
+        if not any((self._changes, self._changelog)):
+            raise ValueError("context requires at least one of changes or changelog")
+
+    @property
+    def changes(self) -> deb822.Changes:
+        if not self._changes:
+            self.lint_error("missing context for changes file")
+        return self._changes
+
+    @property
+    def changelog_entry(self) -> changelog.ChangeBlock:
+        if not self._changelog:
+            self.lint_error("missing context for changelog entry")
+        return self._changelog[0]
+
+    def lint_fail(self, msg: str):
+        raise LintFailure(msg)
+
+    def lint_error(self, msg: str):
+        raise RuntimeError(msg)
